18718d92c864d7aece4ad79c44b08edc24063c8d
slav <my_990@bk.ru>
2019-11-14 17:11:03 +0300

    1111

--- nmonitor.dpr
+++ nmonitor.dpr
@@ -4,8 +4,8 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  Net.Socket in '..\relictum.node\Net\Net.Socket.pas',
-  Net.StreamSocket in '..\relictum.node\Net\Net.StreamSocket.pas',
+  Net.Socket in '..\relictum.node\Library\Net.Socket.pas',
+  Net.StreamSocket in '..\relictum.node\Library\Net.StreamSocket.pas',
   UCustomMemoryStream in '..\relictum.node\Library\UCustomMemoryStream.pas',
   Unit2 in 'Unit2.pas' {ItemFrame: TFrame};
 
